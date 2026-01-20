504546e2aa42d2044d5e4e44291dcb23792ab47f
voltti-cloud <voltti.cloud@gmail.com>
2026-01-20 17:02:15 -0300

    Limpeza total de layout bugado

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-const DB = {
-    filmes: [
-        { t: "A Culpa é das Estrelas", c: "https://image.tmdb.org/t/p/w300/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "https://motor.voltti.cloud/stream/7?hash=52d003" },
-        { t: "Uma Noite no Museu", c: "https://image.tmdb.org/t/p/w300/v9Qp8A058R5R0oP0vLpA6IunY2R.jpg", u: "https://motor.voltti.cloud/stream/8?hash=b328e3" }
-    ],
-    series: [], doramas: []
-};
+const filmes = [
+    { n: "A Culpa é das Estrelas", i: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "https://motor.voltti.cloud/stream/7?hash=52d003" },
+    { n: "Uma Noite no Museu", i: "https://image.tmdb.org/t/p/w500/v9Qp8A058R5R0oP0vLpA6IunY2R.jpg", u: "https://motor.voltti.cloud/stream/8?hash=b328e3" }
+];
 
-function render(cat) {
-    const g = document.getElementById('grid');
-    const data = DB[cat] || [];
-    document.getElementById('view-title').innerText = cat;
-    g.innerHTML = data.length ? data.map(i => `<div class="card" style="background-image:url('${i.c}')" onclick="play('${i.t}','${i.u}')"></div>`).join('') : '<p style="color:#333;grid-column:1/4;text-align:center;margin-top:50px;">Em breve...</p>';
+function init() {
+    const list = document.getElementById('movie-list');
+    list.innerHTML = filmes.map(f => `
+        <div class="card" style="background-image: url('${f.i}')" onclick="play('${f.u}')"></div>
+    `).join('');
 }
 
-function play(t, u) {
-    document.getElementById('p-title').innerText = t;
-    document.getElementById('p-cont').innerHTML = `<video controls autoplay playsinline><source src="${u}" type="video/mp4"></video>`;
-    document.getElementById('player').style.display = 'flex';
+function play(url) {
+    document.getElementById('player-wrap').innerHTML = `
+        <video controls autoplay playsinline><source src="${url}" type="video/mp4"></video>`;
+    document.getElementById('video-overlay').style.display = 'flex';
 }
 
-function closeP() {
-    document.getElementById('p-cont').innerHTML = '';
-    document.getElementById('player').style.display = 'none';
+function closeVid() {
+    document.getElementById('player-wrap').innerHTML = '';
+    document.getElementById('video-overlay').style.display = 'none';
 }
 
-function tab(cat, el) {
-    document.querySelectorAll('.tab-item').forEach(i => i.classList.remove('active'));
-    el.classList.add('active');
-    render(cat);
-}
-
-window.onload = () => render('filmes');
+window.onload = init;
