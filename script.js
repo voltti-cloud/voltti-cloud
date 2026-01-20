a119079debc69d83a5ce90d52eb25d623b154c74
voltti-cloud <voltti.cloud@gmail.com>
2026-01-20 08:30:56 -0300

    FIX: Correção de sintaxe e carregamento de vídeo

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let vltPlayer;
 
-// Inicializa o Plyr apenas uma vez
 function initPlayer() {
     vltPlayer = new Plyr('#player', {
-        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
+        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
         autoplay: false
     });
 }
 
 const DB = {
-    filmes: [{ t: "A Culpa é das Estrelas", c: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "http://motor.voltti.cloud/stream/6?hash=9555df" }]
+    filmes: [{ 
+        t: "A Culpa é das Estrelas", 
+        c: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", 
+        u: "http://motor.voltti.cloud/stream/6?hash=9555df" 
+    }],
+    series: []
 };
 
 function render() {
     const list = document.getElementById('list-movies');
-    if(list) list.innerHTML = DB.filmes.map(i => `
-        <div class="movie-card" style="background-image: url('${i.c}')" onclick="openPlayer('${i.t}', '${i.u}')"></div>
-    `).join('');
+    if(list) {
+        list.innerHTML = DB.filmes.map(i => `
+            <div class="movie-card" style="background-image: url('${i.c}')" onclick="openPlayer('${i.t}', '${i.u}')"></div>
+        `).join('');
+    }
     initPlayer();
 }
 
@@ -24,18 +30,20 @@ function openPlayer(title, url) {
     const overlay = document.getElementById('player-overlay');
     document.getElementById('v-title').innerText = title;
     overlay.style.display = 'flex';
-    
-    // Força o carregamento da fonte
+
     vltPlayer.source = {
         type: 'video',
+        title: title,
         sources: [{ src: url, type: 'video/mp4' }]
     };
-    
-    vltPlayer.play();
+
+    setTimeout(() => {
+        vltPlayer.play().catch(e => console.log("Play bloqueado:", e));
+    }, 500);
 }
 
 function closePlayer() {
-    vltPlayer.pause();
+    vltPlayer.stop();
     document.getElementById('player-overlay').style.display = 'none';
 }
 
