a0a9dbacdfdfee6217e1be7fb94f849620e3a679
voltti-cloud <voltti.cloud@gmail.com>
2026-01-20 07:34:27 -0300

    Layout: Player Premium Customizado (Não Nativo)

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
+const plyrPlayer = new Plyr('#player', {
+    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
+    settings: ['quality', 'speed']
+});
+
 const DB = {
-    filmes: [{ t: "A Culpa é das Estrelas", c: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "http://motor.voltti.cloud/stream/6?hash=9555df" }],
-    series: []
+    filmes: [{ t: "A Culpa é das Estrelas", c: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "http://motor.voltti.cloud/stream/6?hash=9555df" }]
 };
 
 function render() {
-    const movies = document.getElementById('list-movies');
-    if(movies) {
-        movies.innerHTML = DB.filmes.map(i => `
-            <div class="movie-card" style="background-image: url('${i.c}')" onclick="playMovie('${i.t}', '${i.u}')"></div>
-        `).join('');
-    }
+    const list = document.getElementById('list-movies');
+    if(list) list.innerHTML = DB.filmes.map(i => `
+        <div class="movie-card" style="background-image: url('${i.c}')" onclick="openPlayer('${i.t}', '${i.u}')"></div>
+    `).join('');
 }
 
-function playMovie(title, url) {
-    const section = document.getElementById('player-section');
-    const target = document.getElementById('video-target');
+function openPlayer(title, url) {
     document.getElementById('v-title').innerText = title;
+    document.getElementById('player-overlay').style.display = 'flex';
     
-    target.innerHTML = `<video id="v-play" controls autoplay playsinline><source src="${url}" type="video/mp4"></video>`;
-    section.style.display = 'block';
-    document.body.style.overflow = 'hidden'; // Trava o scroll do fundo
+    plyrPlayer.source = {
+        type: 'video',
+        sources: [{ src: url, type: 'video/mp4' }]
+    };
+    
+    plyrPlayer.play();
 }
 
 function closePlayer() {
-    document.getElementById('player-section').style.display = 'none';
-    document.getElementById('video-target').innerHTML = '';
-    document.body.style.overflow = 'auto'; // Destrava o scroll
+    plyrPlayer.stop();
+    document.getElementById('player-overlay').style.display = 'none';
 }
 window.onload = render;
