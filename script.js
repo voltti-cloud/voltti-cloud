610dd0cbb76c2e1047751c2e360b13e64c1fd806
voltti-cloud <voltti.cloud@gmail.com>
2026-01-20 17:35:44 -0300

    Correção definitiva dos links das capas

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const DB = {
     filmes: [
-        { t: "A Culpa é das Estrelas", i: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "https://motor.voltti.cloud/stream/7?hash=52d003" },
-        { t: "Uma Noite no Museu", i: "https://image.tmdb.org/t/p/w500/v9Qp8A058R5R0oP0vLpA6IunY2R.jpg", u: "https://motor.voltti.cloud/stream/8?hash=b328e3" }
+        { 
+            t: "A Culpa é das Estrelas", 
+            i: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", 
+            u: "https://motor.voltti.cloud/stream/7?hash=52d003" 
+        },
+        { 
+            t: "Uma Noite no Museu", 
+            i: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/v9Qp8A058R5R0oP0vLpA6IunY2R.jpg", 
+            u: "https://motor.voltti.cloud/stream/8?hash=b328e3" 
+        }
     ],
     series: [], doramas: []
 };
@@ -13,17 +21,23 @@ function toggleMenu() {
 function render(cat = 'filmes') {
     const grid = document.getElementById('movie-grid');
     const data = DB[cat] || [];
-    document.getElementById('cat-title').innerText = cat.charAt(0).toUpperCase() + cat.slice(1);
+    document.getElementById('cat-title').innerText = cat;
     
-    grid.innerHTML = data.length ? data.map(f => `
-        <div class="card" style="background-image: url('${f.i}')" onclick="play('${f.t}', '${f.u}')"></div>
-    `).join('') : '<p style="color:#444; grid-column: 1/4; text-align:center;">Nenhum conteúdo disponível.</p>';
+    // Aqui forçamos a imagem a aparecer ou mostrar um erro no console
+    grid.innerHTML = data.map(f => `
+        <div class="card" 
+             style="background-image: url('${f.i}'); background-color: #222;" 
+             onclick="play('${f.t}', '${f.u}')">
+        </div>
+    `).join('');
 }
 
 function play(title, url) {
     document.getElementById('playing-title').innerText = title;
     document.getElementById('player-wrap').innerHTML = `
-        <video controls autoplay playsinline><source src="${url}" type="video/mp4"></video>`;
+        <video controls autoplay playsinline style="width:100%;">
+            <source src="${url}" type="video/mp4">
+        </video>`;
     document.getElementById('video-overlay').style.display = 'flex';
 }
 
