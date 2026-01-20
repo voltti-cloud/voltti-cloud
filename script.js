3c807b9e7906d034159fa47b7397196fc9048624
voltti-cloud <voltti.cloud@gmail.com>
2026-01-20 07:41:44 -0300

    FIX DEFINITIVO: Player Premium e Layout Limpo

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-const plyrPlayer = new Plyr('#player', {
-    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'settings', 'fullscreen'],
-    settings: ['quality', 'speed']
-});
+let vltPlayer;
 
 const DB = {
     filmes: [{ t: "A Culpa é das Estrelas", c: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", u: "http://motor.voltti.cloud/stream/6?hash=9555df" }]
 };
 
-function render() {
+function initApp() {
+    // Renderiza as capas
     const list = document.getElementById('list-movies');
     if(list) list.innerHTML = DB.filmes.map(i => `
         <div class="movie-card" style="background-image: url('${i.c}')" onclick="openPlayer('${i.t}', '${i.u}')"></div>
     `).join('');
+
+    // Inicializa o Player Premium
+    vltPlayer = new Plyr('#player', {
+        controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
+        tooltips: { controls: true, seek: true }
+    });
 }
 
 function openPlayer(title, url) {
     document.getElementById('v-title').innerText = title;
     document.getElementById('player-overlay').style.display = 'flex';
     
-    plyrPlayer.source = {
+    vltPlayer.source = {
         type: 'video',
         sources: [{ src: url, type: 'video/mp4' }]
     };
     
-    plyrPlayer.play();
+    vltPlayer.play();
 }
 
 function closePlayer() {
-    plyrPlayer.stop();
+    vltPlayer.stop();
     document.getElementById('player-overlay').style.display = 'none';
 }
-window.onload = render;
+
+window.onload = initApp;
