93fda42999619c24a092fddef0d694bef03b0ce9
voltti-cloud <voltti.cloud@gmail.com>
2026-01-20 12:07:28 -0300

    Adicionado Uma Noite no Museu e ajuste de áudio

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const filmes = [
         titulo: "A Culpa é das Estrelas", 
         capa: "https://image.tmdb.org/t/p/w500/uDsv9LkwN6EH3SBFQDE3uHJyvY6.jpg", 
         url: "https://motor.voltti.cloud/stream/7?hash=52d003" 
+    },
+    { 
+        titulo: "Uma Noite no Museu", 
+        capa: "https://image.tmdb.org/t/p/w500/v9Qp8A058R5R0oP0vLpA6IunY2R.jpg", 
+        url: "https://motor.voltti.cloud/stream/8?hash=b328e3" 
     }
 ];
 
@@ -17,21 +22,14 @@ function render() {
 
 function openPlayer(title, url) {
     document.getElementById('video-title').innerText = title;
-    
-    // Adicionamos 'crossorigin' e removemos o autoplay agressivo
-    // Isso ajuda o navegador a carregar todos os 'tracks' de áudio
     document.getElementById('player-container').innerHTML = `
         <video id="main-video" controls crossorigin="anonymous" style="width:100%; border-radius:10px; background: #000;">
             <source src="${url}" type="video/mp4">
-            Seu navegador não suporta este formato de áudio/vídeo.
         </video>`;
     
     document.getElementById('video-overlay').classList.add('active');
-    
     const v = document.getElementById('main-video');
     v.volume = 1.0;
-    
-    // Tenta carregar explicitamente
     v.load();
 }
 
