b55baedb611123c7956f2ecf4db33d0bef70ccc8
voltti-cloud <voltti.cloud@gmail.com>
2026-01-21 08:16:17 -0300

    Adicionando todos os filmes da planilha oficial

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const DB = {
     filmes: [
-        { t: "A Culpa é das Estrelas", i: "capa1.webp", u: "https://motor.voltti.cloud/stream/7?hash=52d003" },
-        { t: "Uma Noite no Museu 1", i: "capa2.webp", u: "https://motor.voltti.cloud/stream/8?hash=b328e3" }
+        { t: "Crepusculo 1", i: "crepusculo1.webp", u: "https://motor.voltti.cloud/stream/18?hash=c08e43" },
+        { t: "Lua Nova", i: "luanova.webp", u: "https://motor.voltti.cloud/stream/19?hash=32c817" },
+        { t: "Homem Aranha 1", i: "homemaranha1.webp", u: "https://motor.voltti.cloud/stream/14?hash=b15349" },
+        { t: "Homem Aranha 2", i: "homemaranha2.webp", u: "https://motor.voltti.cloud/stream/15?hash=43e939" },
+        { t: "Homem Aranha 3", i: "homemaranha3.webp", u: "https://motor.voltti.cloud/stream/16?hash=0a10b3" },
+        { t: "Círculo de Fogo", i: "circulodefogo1.webp", u: "https://motor.voltti.cloud/stream/9?hash=a51334" },
+        { t: "Círculo de Fogo 2", i: "circulodefogo2.webp", u: "https://motor.voltti.cloud/stream/10?hash=3ca7e5" },
+        { t: "5 Passos de Você", i: "5passos.webp", u: "https://motor.voltti.cloud/stream/12?hash=1e921d" },
+        { t: "Eclipse", i: "eclipse.webp", u: "https://motor.voltti.cloud/stream/20?hash=4d8896" },
+        { t: "Amanhecer Parte 1", i: "amanhecer1.webp", u: "" },
+        { t: "Amanhecer Parte 2", i: "amanhecer2.webp", u: "https://motor.voltti.cloud/stream/21?hash=a7a7fc" }
     ],
     series: [], doramas: []
 };
@@ -9,7 +18,6 @@ const DB = {
 function render(cat = 'filmes') {
     const grid = document.getElementById('movie-grid');
     const data = DB[cat] || [];
-    
     grid.innerHTML = data.map(f => `
         <div class="card" style="background-image: url('${f.i}');" onclick="play('${f.t}', '${f.u}')">
             <div class="card-title">${f.t}</div>
@@ -18,6 +26,7 @@ function render(cat = 'filmes') {
 }
 
 function play(title, url) {
+    if(!url) { alert('Link de streaming não encontrado para este filme!'); return; }
     document.getElementById('playing-title').innerText = title;
     document.getElementById('player-wrap').innerHTML = `
         <video controls autoplay style="width:100%; border-radius:8px;">
